9f8258d5fc98a851d47888a88af629571e19d197
Sasha WILK <sasha.wilk@ecoles-epsi.net>
2025-02-28 11:00:49 +0100

    TP1

---
+++ src/main/java/fr/epsi/ConnexionJpa.java
@@ -0,0 +1,18 @@
+package fr.epsi;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class ConnexionJpa {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("comptaPU");
+
+        EntityManager em = emf.createEntityManager();
+
+        System.out.println("Connexion à la base de données réussie !");
+
+        em.close();
+        emf.close();
+    }
+}
